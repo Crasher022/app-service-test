add3d9645854ce3a6c44850ec82fee8df8b082c7
crasher022 <priminescu.ad@gmail.com>
2025-05-31 18:31:26 +0200

    second deployment of dotnet app

--- Program.cs
+++ Program.cs
@@ -2,5 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello from .NET 8 running on Azure App Service!");
+app.MapGet("/", () => "This is release V 1.1");
+app.MapGet("/", () => "We are on our second deployment");
 
 app.Run();
--- obj/Debug/net8.0/DotNetCore8DemoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNetCore8DemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetCore8DemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c8923a7b7b65cae1db9c08bacf07b31e2299096")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetCore8DemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetCore8DemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
