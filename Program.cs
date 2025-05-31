12dbf3976663fe03611dfbcafac3851c8fe81b0c
crasher022 <priminescu.ad@gmail.com>
2025-05-31 19:06:09 +0200

    v1.5

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ app.MapGet("/", () => @"
     <html>
         <body>
             <h2>Hello from .NET 8 running on Azure App Service!</h2>
-            <p>This is release V 1.1</p>
-            <p>We are on our second deployment</p>
+            <p>This is release V 1.5</p>
+            <p>We are on our third deployment</p>
         </body>
     </html>
 ");
