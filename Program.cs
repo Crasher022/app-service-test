92a3b9eaa5f78213ed490cce8a3cf46111877bc2
crasher022 <priminescu.ad@gmail.com>
2025-05-31 19:16:30 +0200

    v1.6

--- Program.cs
+++ Program.cs
@@ -2,13 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => @"
-    <html>
-        <body>
-            <h2>Hello from .NET 8 running on Azure App Service!</h2>
-            <p>This is release V 1.5</p>
-            <p>We are on our third deployment</p>
-        </body>
-    </html>
+    Hello from .NET 8 running on Azure App Service!
+    This is release V 1.6
 ");
+
 
 app.Run();
